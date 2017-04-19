f1a60459a248f0cbcc309fc5594a9348c5b82bf8
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-04-19 03:46:26 -0500

    add Offset class

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
-from sqlalchemy import Column, Integer, ForeignKey, Float, Boolean, String
-from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class Character(Base):
     __tablename__ = 'characters'
@@ -19,6 +20,18 @@ class Character(Base):
 class Alias(Base):
     __tablename__ = 'aliases'
     id = Column(Integer, primary_key=True)
+
+    alias = Column(String)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship("Character", back_populates="aliases")
+
+
+class Offset(Base):
+    __tablename__ = 'offsets'
+    id = Column(Integer, primary_key=True)
+
+    sentence_offset = Column(Integer)
+    word_offset = Column(Integer)
 
     character_id = Column(Integer, ForeignKey('characters.id'))
     character = relationship("Character", back_populates="aliases")
