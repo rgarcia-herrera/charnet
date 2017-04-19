e55652dca6fb3fc8975182e70207d7f4e7d09b67
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-04-19 03:46:15 -0500

    done word and sentence skip

--- create_character_dict.py
+++ create_character_dict.py
@@ -39,63 +39,88 @@ model.session = session
 class Tagger:
 
     def __init__(self, stdscr, text):
-        self.text = text
         self.screen = stdscr
-        self.tree = parsetree(text)
-        self.current_sentence = 0
-        self.current_word = 0
-        self.word = self.tree.words[0]
 
-    def get_sentence(self):
-        return self.tree[self.current_sentence].string
+        self.tree = parsetree(text)
+
+        self.current_sentence = 0
+        self.sentence = self.tree[0]
+
+        self.current_word = 0
+        self.word = self.sentence.words[0]
+
+        # display position
+        self.pos = 0
 
     def next_word(self):
-        if self.current_word < len(self.tree.words):
+        if self.current_word + 1 < len(self.sentence.words):
             self.current_word += 1
-            self.word = self.tree.words[self.current_word]
-            self.current_sentence = list(
-                self.tree.sentences).index(
-                    self.word.sentence)
+            self.word = self.sentence.words[self.current_word]
+        elif self.current_word + 1 == len(self.sentence.words):
+            self.next_sentence()
 
     def prev_word(self):
         if self.current_word > 0:
             self.current_word -= 1
-            self.word = self.tree.words[self.current_word]
-            self.current_sentence = list(
-                self.tree.sentences).index(
-                    self.word.sentence)
+            self.word = self.sentence.words[self.current_word]
+        else:
+            self.prev_sentence()
 
-            
-    def next_character(self):
-        self.current_sentence += 1
-        while 'NNP' not in \
-              list(self.tree[self.current_sentence].parts_of_speech) \
-              and self.current_sentence < len(self.tree.sentences):
+    def next_sentence(self):
+        if self.current_sentence < len(self.tree.sentences):
             self.current_sentence += 1
+            self.current_word = 0
 
-    def get_pos(self):
-        return "sentence %s, of %s (%0.2d%%)" % (self.current_sentence,
-                                                 len(self.tree.sentences),
-                                                 (self.current_sentence * 100.0)
-                                                 / len(self.tree.sentences))
+        self.sentence = self.tree[self.current_sentence]
+        self.word = self.sentence.words[self.current_word]
+
+    def prev_sentence(self):
+        if self.current_sentence > 0:
+            self.current_sentence -= 1
+            self.sentence = self.tree[self.current_sentence]
+            self.current_word = len(self.sentence.words) - 1
+            self.word = self.sentence.words[self.current_word]
+
+    def next_character(self):
+        self.next_sentence()
+        while 'NNP' not in \
+              list(self.sentence.parts_of_speech):
+            self.next_sentence()
+            # TODO: shift current word to NNP
+            # TODO: ignore previously found characters
+
+    def get_word_pos(self):
+        wordchars = 0
+        for i in range(0, self.word.index):
+            wordchars += len(self.sentence.words[i].string) + 1
+
+#        spaces = self.sentence.string.count(' ', pos, pos)
+
+        return wordchars
 
     def render_sentences(self):
-        sentence = self.get_sentence()
         # show sentence
         self.screen.addstr(3, 0,
-                           sentence,
+                           self.sentence.string,
                            curses.color_pair(2))
 
         # color current word
-        x = sentence.index(self.word.string)
-        self.screen.addstr(3, x,
+        self.screen.addstr(3, self.get_word_pos(),
                            self.word.string,
                            curses.color_pair(3))
 
     def render_pos(self):
+        pos = "sentence %s, of %s (%0.2d%%) word in sentence: %s of %s" \
+              % \
+              (self.current_sentence,
+               len(self.tree.sentences),
+               (self.current_sentence * 100.0) / len(self.tree.sentences),
+               self.current_word + 1,
+               len(self.sentence.words))
+
         # print sentence number
         self.screen.addstr(0, 0,
-                           self.get_pos(),
+                           pos,
                            curses.color_pair(1))
 
     def render(self):
@@ -110,7 +135,7 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
-    stdscr.nodelay(1)
+    # stdscr.nodelay(1)
 
     t = Tagger(stdscr,
                args.text.read())
@@ -134,12 +159,21 @@ def main(stdscr):
             t.prev_word()
             t.screen.clear()
 
+        elif c == ord('N'):
+            t.next_sentence()
+            t.screen.clear()
+
+        elif c == ord('P'):
+            t.prev_sentence()
+            t.screen.clear()
+
         elif c == curses.KEY_HOME:
             t.current_sentence = 0
 
         elif c == ord('q'):
             break  # Exit the while()
 
-        sleep(0.33)
+#        sleep(0.133)
+
 
 curses.wrapper(main)
