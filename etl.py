ffeffc55e20d8e9047ac889986d75579ff924934
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-04-19 21:40:00 -0500

    load parse tree into rdbm

---
+++ etl.py
@@ -0,0 +1,58 @@
+import argparse
+from pattern.en import parsetree
+from time import sleep
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from textwrap import TextWrapper
+import model
+
+description = """Extract using pattern, transform into character model, load to a
+RDBM"""
+
+################################
+# parse command line arguments #
+################################
+parser = argparse.ArgumentParser(
+    description=description)
+
+parser.add_argument('--db_url',
+                    default='sqlite:///characters.sqlite',
+                    help='DB URL, default: sqlite:///characters.sqlite')
+
+parser.add_argument('--text',
+                    type=argparse.FileType('r'), required=True,
+                    help='a plain text file')
+
+args = parser.parse_args()
+
+wrapper = TextWrapper(break_long_words=False)
+
+####################
+# database connect #
+####################
+engine = create_engine(args.db_url)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+model.Base.metadata.create_all(engine)
+model.session = session
+
+tree = parsetree(args.text.read())
+
+wid = 1
+for n in range(len(tree.sentences)):
+    s = tree[n]
+    sentence = session.query(model.Sentence).get(s.id)
+    if sentence is None:
+        sentence = model.Sentence(id=s.id,
+                                  sentence=s.string)
+        session.add(sentence)
+
+    for w in s.words:
+        session.add(model.Word(id=wid,
+                               word=w.string,
+                               pos=w.pos,
+                               sentence=sentence))
+        wid += 1
+
+session.commit()
--- model.py
+++ model.py
@@ -26,12 +26,37 @@ class Alias(Base):
     character = relationship("Character", back_populates="aliases")
 
 
-class Offset(Base):
-    __tablename__ = 'offsets'
+# class Offset(Base):
+#     __tablename__ = 'offsets'
+#     id = Column(Integer, primary_key=True)
+
+#     sentence_offset = Column(Integer)
+#     word_offset = Column(Integer)
+
+#     character_id = Column(Integer, ForeignKey('characters.id'))
+#     character = relationship("Character", back_populates="aliases")
+
+
+class Word(Base):
+    __tablename__ = 'words'
     id = Column(Integer, primary_key=True)
+    word = Column(String)
+    pos = Column(String)
 
-    sentence_offset = Column(Integer)
-    word_offset = Column(Integer)
+    sentence_id = Column(Integer, ForeignKey('sentences.id'))
+    sentence = relationship("Sentence", back_populates="words")
 
-    character_id = Column(Integer, ForeignKey('characters.id'))
-    character = relationship("Character", back_populates="aliases")
+    def __repr__(self):
+        return "<w%s %s %s>" % (self.id, self.pos, self.word)
+
+
+class Sentence(Base):
+    __tablename__ = 'sentences'
+    id = Column(Integer, primary_key=True)
+    sentence = Column(String)
+    words = relationship("Word", back_populates="sentence")
+
+    def __repr__(self):
+        return "<s%s '%s'>" % (self.id, self.sentence)
+
+    
