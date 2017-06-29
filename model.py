dd4a03c8883668002367e6af4e3821e46f74dd3d
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-06-28 22:13:13 -0500

    seeking chars and names

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy import Column, Integer, ForeignKey, String, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -20,34 +20,45 @@ class Character(Base):
     def __repr__(self):
         return "<chr%s %s %s>" % (self.id, self.name, self.aliases)
 
-    def seek_next_unmarked(self):
-        if ' ' in self.name:
-            parts = self.name.split(' ')
-            w = session.query(
-                Word).filter(
-                    Word.word.like('%'+parts[0]+'%')).filter(
-                        Word.character == None
-                    ).first()
-            if w is None:
-                return []
+    def from_words(self, words):
+        self.name = " ".join([word.word for word in words])
+        self.words = words
+
+    def get_names(self):
+        return [self.name, ] + [alias.alias for alias in self.aliases]
+
+    def seek_unmarked_word(self):
+        for name in self.get_names():
+            if ' ' in name:
+                parts = name.split(' ')
+                w = session.query(
+                    Word).filter(
+                        Word.word.like('%'+parts[0]+'%')).filter(
+                            Word.character == None
+                        ).filter(
+                            Word.ignore == False
+                        ).first()
+                if w is None:
+                    return []
+                else:
+                    return [w, ] + [session.query(Word).get(w.id + n)
+                                    for n in range(1, len(parts))]
             else:
-                return [w, ] + [session.query(Word).get(w.id + n)
-                                for n in range(1, len(parts))]
-        else:
-            w = session.query(
-                Word).filter(
-                    Word.word.like('%'+self.name+'%')).filter(
-                        Word.character == None
-                    ).first()
-            if w:
-                return [w, ]
-            else:
-                return []
+                w = session.query(
+                    Word).filter(
+                        Word.word.like('%'+self.name+'%')).filter(
+                            Word.character == None
+                        ).filter(
+                            Word.ignore == False
+                        ).first()
+                if w:
+                    return [w, ]
+                else:
+                    return []
 
     def mark_all(self):
-        while self.seek_next_unmarked():
-            for w in self.seek_next_unmarked():
-                print w
+        while self.seek_unmarked_word():
+            for w in self.seek_unmarked_word():
                 w.character = self
                 session.commit()
 
@@ -57,7 +68,7 @@ class Alias(Base):
     id = Column(Integer, primary_key=True)
 
     alias = Column(String)
-    character_id = Column(Integer, ForeignKey('characters.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
     character = relationship("Character", back_populates="aliases")
 
     def __repr__(self):
@@ -69,6 +80,8 @@ class Word(Base):
     id = Column(Integer, primary_key=True)
     word = Column(String)
     pos = Column(String)
+
+    ignore = Column(Boolean, default=False)
 
     sentence_id = Column(Integer, ForeignKey('sentences.id'))
     sentence = relationship("Sentence", back_populates="words")
@@ -103,3 +116,25 @@ class Sentence(Base):
             w.y = y
 
             offset += len(w.word) + 1
+
+
+def seek_unmarked_name():
+    w = session.query(Word).filter((Word.pos == 'NNP')
+                                   | (Word.pos == 'NNPS'))\
+                           .filter(Word.character == None)\
+                           .filter(Word.ignore == False)\
+                           .first()
+
+    if w is None:
+        return []
+    else:
+        nnp = [w, ]
+        n = 1
+        nextw = session.query(Word).get(w.id + n)
+        while n < session.query(Word).count() and \
+                nextw.pos == 'NNP' or nextw.pos == 'NNPS':
+            nnp.append(nextw)
+            n += 1
+            nextw = session.query(Word).get(w.id + n)
+
+        return nnp
